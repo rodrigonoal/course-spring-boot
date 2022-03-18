a9dd9f01da3e1fb60124886b7849308a8fc07e11
Rodrigo Moraes Noal <rodrigonoal@gmail.com>
2022-03-17 21:49:11 -0300

    spring configs for h2 added

--- src/main/java/com/nelioalves/cursomc/domain/Category.java
+++ src/main/java/com/nelioalves/cursomc/domain/Category.java
@@ -2,17 +2,24 @@ package com.nelioalves.cursomc.domain;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Getter
     @Setter
     @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //definindo a estratégia de geração automática (depende do database)
     private Integer id;
     @Getter
     @Setter
