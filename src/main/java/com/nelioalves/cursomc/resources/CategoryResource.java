a5421be433bd431e7fc8862acc65ab12d1485772
Rodrigo Moraes Noal <rodrigonoal@gmail.com>
2022-03-17 22:24:23 -0300

    layers created

--- src/main/java/com/nelioalves/cursomc/resources/CategoryResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoryResource.java
@@ -9,9 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController //controlador
 @RequestMapping(value = "/categories") //nomear o endpoint
 public class CategoryResource {
