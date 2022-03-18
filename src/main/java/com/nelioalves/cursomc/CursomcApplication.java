0a6e8650547395eedbd68b066154b5ba6a261490
Rodrigo Moraes Noal <rodrigonoal@gmail.com>
2022-03-18 19:03:05 -0300

    added default values to db

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.nelioalves.cursomc;
 
+import com.nelioalves.cursomc.domain.Category;
+import com.nelioalves.cursomc.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
     }
 
+    //command line runner permite que ações ocorram no momento da criação da aplicação
+    // para tanto devemos implementar o método run com override
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Informatics");
+        Category cat2 = new Category(null, "Office");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+    }
 }
--- src/main/java/com/nelioalves/cursomc/domain/Category.java
+++ src/main/java/com/nelioalves/cursomc/domain/Category.java
@@ -1,7 +1,6 @@
 package com.nelioalves.cursomc.domain;
 
 import lombok.*;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
